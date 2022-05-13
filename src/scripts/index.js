3d04344a65795b67919e3ebbf2dd1a2f3fc12b61
Md.Tarikul Islam Juel <md.tarikulislamjuel@gmail.com>
2022-05-13 16:13:40 +0600

    Add files via upload

---
+++ src/scripts/index.js
@@ -0,0 +1,8 @@
+import "../styles/styles.css";
+
+const add=require("./add");
+const sub=require("./sub");
+
+let  result=add(1,2)
+
+console.log("result = "+result)
---
+++ webpack.config.js
@@ -0,0 +1,97 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const config = {
+  entry: './src/scripts/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js'
+  },
+  mode:'development',
+  devServer:{
+    port:2222,
+    open:true,
+    compress:true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        exclude: [/node_modules/, require.resolve('./src/index.html')],
+        use: [
+          {
+            loader: 'file-loader',
+          },
+          {
+            loader: 'html-loader',
+          },
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ],
+        exclude: /\.module\.css$/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true
+            }
+          }
+        ],
+        include: /\.module\.css$/
+      },
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "images/",
+              publicPath: "images/"
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    }),
+  ],
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
+    }
+  }
+};
+
+module.exports = config;
